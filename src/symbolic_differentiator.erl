2342a7badf0378218e2d22aa5923280d7fa36d73
Shanti Chellaram <shanti@teacherspayteachers.com>
2019-03-11 00:58:45 -0400

    Add derivative code
    This adds derivatives for many of the built-in functions. This
    introduces a bug, as well. The derivative for the exponentiation
    function only uses the constant power rule and not the general power
    rule, since there is no support currently for logarithm functions.

--- src/symbolic_differentiator.erl
+++ src/symbolic_differentiator.erl
@@ -11,11 +11,14 @@
 main(Args) ->
     io:format("Args: ~p~n", [Args]),
     case Args of
-        [Input] ->
+        [Input, [VarName]] ->
             {ok, Tokens} = sd_lexer:tokenize(Input),
             io:format("Lex: ~p~n", [Tokens]),
             {ok, AST} = sd_parser:parse(Tokens),
-            io:format("AST: ~p~n", [AST]);
+            io:format("AST: ~p~n", [AST]),
+            Var = VarName - $a,
+            Derivative = derivative(AST, Var),
+            io:format("Derivative: ~p~n", [Derivative]);
         _ ->
             io:format("No input provided.")
     end,
@@ -25,3 +28,16 @@ main(Args) ->
 %% Internal functions
 %%====================================================================
 
+derivative({'+', Lhs, Rhs}, V) -> {'+', derivative(Lhs, V), derivative(Rhs, V)};
+derivative({'-', Lhs, Rhs}, V) -> {'-', derivative(Lhs, V), derivative(Rhs, V)};
+derivative({'*', Lhs, Rhs}, V) -> {'+', {'*', derivative(Lhs, V), Rhs}, {'*', Lhs, derivative(Rhs, V)}};
+derivative({'/', Lhs, Rhs}, V) -> {'/', {'-', {'*', derivative(Lhs, V), Rhs}, {'*', Lhs, derivative(Rhs, V)}}, {exp, Rhs, {const, 2}}};
+%derivative({exp, {variable, V} = Lhs, Rhs}, V) -> {'*', Rhs, {exp, Lhs, {'-', Rhs, {const, 1}}}};
+derivative({exp, Lhs, Rhs}, V) -> {'*', {'*', Rhs, {exp, Lhs, {'-', Rhs, {const, 1}}}}, derivative(Lhs, V)};
+derivative({variable, V}, V) -> {const, 1};
+derivative({variable, _NotV}, _V) -> {const, 0};
+derivative({const, _Number}, _V) -> {const, 0}.
+
+%% TODO: Reducer
+%% TODO: Swap specific power rule for general power rule
+%% TODO: Pretty printer for reduced itmes
